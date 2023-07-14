a44ebff7c327e1f3f88efee740dc179a87ae4ab8
bfren <bf@bfren.dev>
2023-07-14 08:20:28 +0100

    Moving construct to act section

--- tests/Tests.Rqlite.Client/Internals/JsonContent/ConstructorTests.cs
+++ tests/Tests.Rqlite.Client/Internals/JsonContent/ConstructorTests.cs
@@ -11,39 +11,36 @@ public class ConstructorTests
 		// Arrange
 		var v0 = Rnd.Str;
 		var v1 = Rnd.Int;
-		var content = new JsonContent(new { v0, v1 });
 		var expected = $"{{\"v0\":\"{v0}\",\"v1\":{v1}}}";
 
 		// Act
-		var result = content.ReadAsStringAsync().Result;
+		var result = new JsonContent(new { v0, v1 });
 
 		// Assert
-		Assert.Equal(expected, result);
+		Assert.Equal(expected, result.ReadAsStringAsync().Result);
 	}
 
 	[Fact]
 	public void uses_utf8_charset()
 	{
 		// Arrange
-		var content = new JsonContent(Rnd.Str);
 
 		// Act
-		var result = content.Headers.ContentType?.CharSet;
+		var result = new JsonContent(Rnd.Str);
 
 		// Assert
-		Assert.Equal("utf-8", result);
+		Assert.Equal("utf-8", result.Headers.ContentType?.CharSet);
 	}
 
 	[Fact]
 	public void uses_json_media_type()
 	{
 		// Arrange
-		var content = new JsonContent(Rnd.Str);
 
 		// Act
-		var result = content.Headers.ContentType?.MediaType;
+		var result = new JsonContent(Rnd.Str);
 
 		// Assert
-		Assert.Equal("application/json", result);
+		Assert.Equal("application/json", result.Headers.ContentType?.MediaType);
 	}
 }
