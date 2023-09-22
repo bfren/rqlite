b9d6bce6f7ee1ecf3aa0aa546b3e7755a6f06c67
bfren <bf@bfren.dev>
2023-09-22 15:39:18 +0100

    Refactoring responses to be internal

---
+++ src/Rqlite.Client/Internals/QueryResponseResult.cs
@@ -0,0 +1,22 @@
+// Rqlite client for .NET.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2023
+
+using System.Collections.Generic;
+
+namespace Rqlite.Client.Internals;
+
+/// <summary>
+/// Result properties.
+/// </summary>
+public sealed record class QueryResponseResult<T> : ResponseResult
+{
+	/// <summary>
+	/// Dictionary of column types, where the key is the column name and the value is the type.
+	/// </summary>
+	public Dictionary<string, string>? Types { get; init; }
+
+	/// <summary>
+	/// List of values.
+	/// </summary>
+	public List<T>? Rows { get; init; }
+}
---
+++ src/Rqlite.Client/Internals/ResponseResult.cs
@@ -0,0 +1,15 @@
+// Rqlite client for .NET.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2023
+
+namespace Rqlite.Client.Internals;
+
+/// <summary>
+/// Common fields in Rqlite response results.
+/// </summary>
+public abstract record class ResponseResult
+{
+	/// <summary>
+	/// Set when there has been an error executing a request.
+	/// </summary>
+	public string? Error { get; init; }
+}
